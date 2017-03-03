cd18fcd59719d313dae5f2a8260d9aa204754345
Nathan M. Davis <davis8@rose-hulman.edu>
2017-03-03 15:47:16 -0500

    update

--- process/js/render.js
+++ process/js/render.js
@@ -15,6 +15,9 @@ var MainInterface = React.createClass({
   },//getInitialState
   render: function(){
     var myAppointments = this.state.myAppointments;
+
+    
+
     return(
       <div className="application">
         <div className="container">
@@ -23,17 +26,7 @@ var MainInterface = React.createClass({
              <h2 className="appointments-headline">Current Appointments</h2>
              <ul className="item-list media-list">
 
-               <li className="pet-item media">
-                 <div className="pet-info media-body">
-                   <div className="pet-head">
-                     <span className="pet-name">{myAppointments[0].petName}</span>
-                     <span className="apt-date pull-right">{myAppointments[0].aptDate}</span>
-                   </div>
-                   <div className="owner-name"><span className="label-item">Owner:</span>
-                   {myAppointments[0].ownerName}</div>
-                   <div className="apt-notes">{myAppointments[0].aptNotes}</div>
-                 </div>
-               </li>
+
 
              </ul>
            </div>{/* col-sm-12 */}
